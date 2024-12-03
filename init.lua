99141737f600cc6ede4b3174efc5d4a20bfcb808
Benjamin Alard <88625126+BenAlard@users.noreply.github.com>
2024-12-03 23:31:45 +0100

    switch toggle to focus

--- init.lua
+++ init.lua
@@ -168,7 +168,7 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
 -- Toggle tree
-vim.keymap.set('n', '<leader>t', '<cmd>NvimTreeToggle<CR>')
+vim.keymap.set('n', '<leader>t', '<cmd>NvimTreeFocus<CR>')
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
